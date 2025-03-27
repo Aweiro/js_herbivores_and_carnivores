85b9baeac8bca89ec04694e8f164f205164d195e
Aweiro <arsenskott@gmail.com>
2025-03-27 17:20:09 +0200

    Solution 1

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -8,20 +8,13 @@ class Animal {
     Animal.alive.push(this);
   }
   isAlive(name) {
-    if (name.health === 0) {
+    if (name.health <= 0) {
       Animal.alive = Animal.alive.filter((animal) => animal !== name);
     }
   }
 
   hide() {
     this.hidden = true;
-  }
-
-  bite(name) {
-    if (name.hidden === false) {
-      name.health -= 50;
-      this.isAlive(name);
-    }
   }
 }
 
@@ -30,9 +23,11 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
-  constructor(name) {
-    super(name);
-    super.bite(name);
+  bite(name) {
+    if (name instanceof Herbivore && name.hidden === false) {
+      name.health -= 50;
+      this.isAlive(name);
+    }
   }
 }
 
@@ -41,38 +36,3 @@ module.exports = {
   Herbivore,
   Carnivore,
 };
-
-// const deer = new Herbivore('Bembi');
-// const panther = new Carnivore('Bagira');
-// const lion = new Carnivore('King');
-// const rabbit = new Herbivore('Max');
-
-// // Animal.alive === [
-// //   {name: 'Bembi', health: 100, hidden: false},
-// //   {name: 'Bagira', health: 100},
-// //   {name: 'King', health: 100},
-// //   {name: 'Max', health: 100, hidden: false},
-// // ];
-// console.log(Animal.alive);
-
-// lion.bite(deer);
-// panther.bite(lion);
-
-// // Animal.alive === [
-// //   {name: 'Bembi', health: 50},
-// //   {name: 'Bagira', health: 100},
-// //   {name: 'King', health: 100},
-// //   {name: 'Max', health: 100},
-// // ];
-// console.log(Animal.alive);
-
-// panther.bite(deer);
-// rabbit.hide();
-// panther.bite(rabbit);
-
-// // Animal.alive === [
-// //   {name: 'Bagira', health: 100},
-// //   {name: 'King', health: 100},
-// //   {name: 'Max', health: 100, hidden: true},
-// // ];
-// console.log(Animal.alive);
